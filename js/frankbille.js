e47ab19aaa7bcca26d74991001ddd9f2f4844e88
Frank Bille <github@frankbille.dk>
2012-09-25 21:26:59 +0200

    Removed the GitHub activity stream
    It doesn't really make sense, since it can be seen in full on the
    GitHub website, and would be a pain to maintain.

--- js/frankbille.js
+++ js/frankbille.js
@@ -1,9 +1,3 @@
-function frankbille_log(obj) {
-	if (console && console.log) {
-		console.log(obj);
-	}
-}
-
 $(function() {
 	/**
 	 * Tooltips
@@ -50,89 +44,6 @@ $(function() {
 						
 						repoAdded = true;
 					});
-				}
-			}
-		});
-	});
-	
-	/**
-	 * GitHub activity feed
-	 */
-	$('.github-activity').each(function() {
-		var container = $(this);
-		var user = container.data("user");
-		
-		$.ajax({
-			url: 'https://api.github.com/users/'+user+'/events/public?page=1&per_page=10',
-			dataType: 'jsonp',
-
-			success: function(results){
-				if ($.isArray(results.data)) {
-					// /events api doesn't seem to support per_page
-					// definition, so we always get 30, even though we
-					// only want 10
-					var data = results.data.slice(0, 9);
-					
-					$.each(data, function(index, value) {
-						var repoUrl = "https://github.com/"+value.repo.name;
-
-						var item = $("<div style='margin-bottom:20px'>");
-						var createdAt = moment(value.created_at);
-						item.append($("<time class='muted'>").attr("datetime", value.created_at).livestamp(createdAt));
-						item.append("<br>");
-						
-						if (value.type == "CreateEvent") {
-							if (value.payload.ref_type == "tag") {
-								var strong = $("<strong>");
-								strong.append(value.actor.login + " created tag ");
-								strong.append($("<a>").attr("href", repoUrl+"/tree/"+value.payload.ref).text(value.payload.ref));
-								strong.append(" at ");
-								strong.append($("<a>").attr("href", repoUrl).text(value.repo.name));
-								item.append(strong);
-							} else {
-								frankbille_log("Unsupported create event ref type: "+value.payload.ref_type);
-								item = "";
-							}
-						} else if (value.type == "PublicEvent") {
-							var strong = $("<strong>");
-							strong.append(value.actor.login + " open sources ");
-							strong.append($("<a>").attr("href", repoUrl).text(value.repo.name));
-							item.append(strong);
-						} else if (value.type == "PushEvent") {
-							var strong = $("<strong>");
-							strong.append(value.actor.login + " pushed to ");
-							strong.append($("<a>").attr("href", repoUrl).text(value.repo.name));
-							item.append(strong);
-							var maxCommits = value.payload.commits.length < 3 ? value.payload.commits.length : 3;
-							for (i = 0; i < maxCommits; i++) {
-								var commit = value.payload.commits[i];
-								var c = $("<div class='row'>");
-								c.append($("<div class='span1'>").append($("<small>").append($("<a>").attr("href", repoUrl+"/commit/"+commit.sha).text(commit.sha.substring(0, 8)))));
-								var msg = commit.message;
-								if (msg.indexOf("\n") > -1) {
-									msg = msg.substring(0, msg.indexOf("\n"));
-								}
-								c.append($("<div class='span7'>").append($("<small>").text(msg)));
-								item.append(c);
-							}
-							if (value.payload.commits.length > maxCommits) {
-								item.append($("<a>").attr("href", repoUrl+"/compare/"+value.payload.before+"..."+value.payload.head).text("See all "+value.payload.size+" commits"));
-							}
-						} else if (value.type == "WatchEvent") {
-							var strong = $("<strong>");
-							strong.append(value.actor.login + " starred ");
-							strong.append($("<a>").attr("href", repoUrl).text(value.repo.name));
-							item.append(strong);
-						} else {
-							frankbille_log("Unsupported value type: "+value.type);
-							item = "";
-						}
-						
-						container.append(item);
-					});
-					
-					container.append("<br>");
-					container.append($("<a class='btn btn-small'>").attr("href", "https://github.com/frankbille?tab=activity").text("See more on Github Profile"));
 				}
 			}
 		});
