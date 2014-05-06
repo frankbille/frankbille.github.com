43114865d273abeba683ee62cf4c853732e6c992
Frank Bille <github@frankbille.dk>
2014-05-06 23:44:55 +0200

    Remove logging and clean up the code.

--- app/js/controllers/githubprojects.ctrl.js
+++ app/js/controllers/githubprojects.ctrl.js
@@ -4,11 +4,5 @@ angular.module('frankbilleApp').controller('GitHubProjectsCtrl', function ($scop
   $http.jsonp('https://api.github.com/users/frankbille/repos?callback=JSON_CALLBACK', {responseType: 'json'})
     .success(function (data) {
       $scope.repos = data.data;
-      console.log($scope.repos);
-    })
-    .error(function (data, status) {
-      console.log('ERROR');
-      console.log(data);
-      console.log(status);
     });
 });
